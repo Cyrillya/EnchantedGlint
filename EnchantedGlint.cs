6ac1c8943a841b9e3ea1d997bf646aaf5e3104c9
-Cyril- <59193481@qq.com>
2024-03-09 00:11:24 +0800

    fix error & reformat

--- EnchantedGlint.cs
+++ EnchantedGlint.cs
@@ -8,10 +8,12 @@ using Terraria.ModLoader;
 
 namespace EnchantedGlint;
 
-public class EnchantedGlint : Mod { }
-
 public class EffectGlobalItem : GlobalItem
 {
+    public class EnchantedGlint : Mod
+    {
+    }
+
     internal static readonly HashSet<int> Prefixes = new() {
         PrefixID.Legendary, PrefixID.Legendary2, PrefixID.Godly, PrefixID.Light,
         PrefixID.Demonic, PrefixID.Unreal, PrefixID.Mythical, PrefixID.Ruthless,
@@ -19,27 +21,27 @@ public class EffectGlobalItem : GlobalItem
         PrefixID.Quick, PrefixID.Violent
     };
 
-    public override bool PreDrawInInventory(Item item, SpriteBatch sb, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
-    {
+    public override bool PreDrawInInventory(Item item, SpriteBatch sb, Vector2 position, Rectangle frame,
+        Color drawColor, Color itemColor, Vector2 origin, float scale) {
         if (!Prefixes.Contains(item.prefix)) {
             return true;
         }
 
         var texture = ModContent.Request<Texture2D>("EnchantedGlint/Enchanted");
         var shader = ModContent.Request<Effect>("EnchantedGlint/Transform", AssetRequestMode.ImmediateLoad).Value;
-        
+
         shader.Parameters["uTime"].SetValue(Main.GlobalTimeWrappedHourly * 0.2f);
         shader.CurrentTechnique.Passes["EnchantedPass"].Apply();
         Main.instance.GraphicsDevice.Textures[1] = texture.Value; // 传入调色板
-        
+
         sb.End();
         sb.Begin(SpriteSortMode.Deferred, sb.GraphicsDevice.BlendState, sb.GraphicsDevice.SamplerStates[0],
             sb.GraphicsDevice.DepthStencilState, sb.GraphicsDevice.RasterizerState, shader, Main.UIScaleMatrix);
         return true;
     }
 
-    public override void PostDrawInInventory(Item item, SpriteBatch sb, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
-    {
+    public override void PostDrawInInventory(Item item, SpriteBatch sb, Vector2 position, Rectangle frame,
+        Color drawColor, Color itemColor, Vector2 origin, float scale) {
         if (!Prefixes.Contains(item.prefix)) {
             return;
         }
@@ -49,10 +51,9 @@ public class EffectGlobalItem : GlobalItem
             sb.GraphicsDevice.DepthStencilState, sb.GraphicsDevice.RasterizerState, null, Main.UIScaleMatrix);
     }
 
-    public override bool PreDrawInWorld(SpriteBatch sb, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
-    {
-        if (!Prefixes.Contains(item.prefix))
-        {
+    public override bool PreDrawInWorld(Item item, SpriteBatch sb, Color lightColor, Color alphaColor,
+        ref float rotation, ref float scale, int whoAmI) {
+        if (!Prefixes.Contains(item.prefix)) {
             return true;
         }
 
@@ -64,19 +65,22 @@ public class EffectGlobalItem : GlobalItem
         Main.instance.GraphicsDevice.Textures[1] = texture.Value;
 
         sb.End();
-        sb.Begin(SpriteSortMode.Deferred, Main.spriteBatch.GraphicsDevice.BlendState, sb.GraphicsDevice.SamplerStates[0], 
-            Main.spriteBatch.GraphicsDevice.DepthStencilState, sb.GraphicsDevice.RasterizerState, shader, Main.GameViewMatrix.TransformationMatrix);
+        sb.Begin(SpriteSortMode.Deferred, Main.spriteBatch.GraphicsDevice.BlendState,
+            sb.GraphicsDevice.SamplerStates[0],
+            Main.spriteBatch.GraphicsDevice.DepthStencilState, sb.GraphicsDevice.RasterizerState, shader,
+            Main.GameViewMatrix.TransformationMatrix);
         return true;
     }
 
-    public override void PostDrawInWorld(SpriteBatch sb, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
-    {
+    public override void PostDrawInWorld(Item item, SpriteBatch sb, Color lightColor, Color alphaColor, float rotation,
+        float scale, int whoAmI) {
         if (!Prefixes.Contains(item.prefix)) {
             return;
         }
 
         sb.End();
-        sb.Begin(SpriteSortMode.Deferred, sb.GraphicsDevice.BlendState, sb.GraphicsDevice.SamplerStates[0], 
-            sb.GraphicsDevice.DepthStencilState, sb.GraphicsDevice.RasterizerState, null, Main.GameViewMatrix.TransformationMatrix);
+        sb.Begin(SpriteSortMode.Deferred, sb.GraphicsDevice.BlendState, sb.GraphicsDevice.SamplerStates[0],
+            sb.GraphicsDevice.DepthStencilState, sb.GraphicsDevice.RasterizerState, null,
+            Main.GameViewMatrix.TransformationMatrix);
     }
 }
